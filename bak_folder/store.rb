050e845cc6e1be3ccaed919ea4da3d716f587f42
Jonathan Lai <jonathanlai@eltux.net>
2013-04-07 19:47:36 -0700

    Dropdown menu added

--- bak_folder/store.rb
+++ bak_folder/store.rb
@@ -1,45 +1,59 @@
-# gem install --version 1.3.0 sinatra
-require 'pry'
-gem 'sinatra', '1.3.0'
-require 'sinatra'
-require 'sinatra/reloader'
-require 'sqlite3'
- 
- 
-get '/users' do
-  db = SQLite3::Database.new "store.sqlite3" #create a handle to the database
-  db.results_as_hash = true
-  @rs = db.prepare('SELECT * FROM users;').execute #recordset 
-  erb :show_users #render show users
-end
- 
- 
-get '/' do
-  erb :home
-end
- 
- 
- 
- 
- 
- 
- 
- 
- 
 =begin
- 
-  <form method='post' action='/create'>
-    <input type='text' name='name' autofocus>
-    <input type='text' name='photo'>
-    <input type='text' name='breed'>
-    <button>dog me!</button>
-  </form>
- 
- 
-  post '/create' do
-  end
- 
- 
-  redirect '/'
- 
+
+
+1st
+2nd
+3rd
+4th
+5th
+6th
+7th
+8th
+9th
+10th
+11th
+12th
+13th
+14th
+15th
+16th
+17th
+18th
+19th
+
+20th
+21st
+22nd
+23rd
+24th
+25th
+
+31st
+32nd
+33rd
+34th
+
+
 =end
+
+
+def convert_to_ordinal(number)
+ 
+  y = number.to_s.reverse
+    if y[0] == 1
+      number.to_s + "st"
+    elsif y[0] == 1 && y[1] == 1
+      number.to_s + "th"
+      
+    # elsif y[0] == 2 && y[1] == 1
+    #   number.to_s + "th"     
+    if y[0] == 2 && y[1] != 1
+      number.to_s + "th"
+    elsif y[0] == 3 && y[1] != 1
+      number.to_s + "rd"
+    end
+      number.to_s + "th"
+
+end
+
+convert_to_ordinal(24)
